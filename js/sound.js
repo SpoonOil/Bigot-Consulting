c5d41869dff7fbbd1990000630357b9db35fb0d2
SpoonOil <nieves.jon.8@gmail.com>
2020-08-11 22:44:10 -0400

    continued working on sound js file

--- js/sound.js
+++ js/sound.js
@@ -1,7 +1,8 @@
-console.log('this is runnign');
 function PlaySound(sound) {
   console.log('this is running');
-  var path = "audio/";
+  var path = "../audio/";
   var snd = new Audio(path + sound + ".mp3");
+  snd.status = 'dix';
+  console.log(sound.status);
   snd.play();
 }
