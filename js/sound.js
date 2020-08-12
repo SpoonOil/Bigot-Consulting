cc1ef325831bb07ceef42f5f6b00ef29de100573
SpoonOil <nieves.jon.8@gmail.com>
2020-08-11 22:35:28 -0400

    added sound handler

--- js/sound.js
+++ js/sound.js
@@ -1,4 +1,5 @@
-function PlaySound(sound, level) {
+console.log('this is runnign');
+function PlaySound(sound) {
   console.log('this is running');
   var path = "audio/";
   var snd = new Audio(path + sound + ".mp3");
