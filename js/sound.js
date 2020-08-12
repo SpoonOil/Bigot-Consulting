3dbcd388a43ca81d85aecb76e63131c1d2b035a6
SpoonOil <nieves.jon.8@gmail.com>
2020-08-12 11:12:33 -0400

    downloaded leonie image and edited sound script

--- js/sound.js
+++ js/sound.js
@@ -2,7 +2,5 @@ function PlaySound(sound) {
   console.log('this is running');
   var path = "../audio/";
   var snd = new Audio(path + sound + ".mp3");
-  snd.status = 'dix';
-  console.log(sound.status);
   snd.play();
 }
